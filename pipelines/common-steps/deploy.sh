ab60b54e6e3b2a6d1db864bf31e8077aee61f9ec
Ubuntu <ubuntu@ip-172-31-84-208.ec2.internal>
2023-06-14 18:53:44 +0000

    se agrega parametro staging a mano en deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,5 @@
 
 set -x
 du -hs * | sort -h
-sam deploy --template ../../todo-list-aws/template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+#sam deploy --template ../../todo-list-aws/template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy template.yaml --config-env staging --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name todo-list-aws-staging --region us-east-1 --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
