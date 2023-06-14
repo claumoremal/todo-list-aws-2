8019d4bd42ab8ea6545eb5445dc3344e12a290d3
Claudio Moreno <102189503+claumoremal@users.noreply.github.com>
2023-06-14 17:14:43 -0300

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -3,4 +3,4 @@
 set -x
 du -hs * | sort -h
 #sam deploy -t todo-list-aws/template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
-sam deploy template.yaml --config-env staging --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name todo-list-aws-staging --region us-east-1 --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name todo-list-aws-staging --region us-east-1 --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
