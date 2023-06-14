24f5ff113d0eab8cae5ebfe87dc4269af7ab90b6
Claudio Moreno <102189503+claumoremal@users.noreply.github.com>
2023-06-14 15:27:41 -0300

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,5 @@
 
 set -x
 du -hs * | sort -h
-sam deploy -t todo-list-aws/template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+#sam deploy -t todo-list-aws/template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name todo-list-aws-staging-pipeline
