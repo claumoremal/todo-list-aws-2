31d056ba0f131a5a56dd5ecd27f8bc619a7ae73a
Ubuntu <ubuntu@ip-172-31-84-208.ec2.internal>
2023-06-17 13:22:18 +0000

    agregados parametros de region y nombre y rollback de stack en deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,5 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production 
+-s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30 --on-failure ROLLBACK
