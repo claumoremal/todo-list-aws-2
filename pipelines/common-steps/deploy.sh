922bbc3229f14f45d7068d288b909b7ea1c92a94
Ubuntu <ubuntu@ip-172-31-84-208.ec2.internal>
2023-06-19 13:26:25 +0000

    nueva mpodificacion en deploy

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --resolve-s3 --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
