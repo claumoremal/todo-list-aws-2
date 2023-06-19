500090520d87fcf59cc108c4d18adfce9d556e48
Ubuntu <ubuntu@ip-172-31-84-208.ec2.internal>
2023-06-19 13:03:50 +0000

    se agrega s3 bucket en samconfig.toml entorno production

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,5 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production 
--s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30 --on-failure ROLLBACK
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --stack-name todo-list-aws-production
