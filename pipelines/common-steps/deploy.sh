04a18a1e72c4254d4eadee09ec94f1ab9e95d222
Ubuntu <ubuntu@ip-172-31-84-208.ec2.internal>
2023-06-19 19:33:45 +0000

    commit

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
+sam deploy template.yaml --config-env ${ENVIRONMENT} --stack-name todo-list-aws-${ENVIRONMENT} ---no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --region us-east-1 --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-c5ep64ophu30
